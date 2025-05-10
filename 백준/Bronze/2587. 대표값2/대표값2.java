5b1d7590cc0a6fd4b2c0904c9a330cf3ed2f540e
JeongJaeYoon <jyoon3741@naver.com>
2025-05-10 22:30:09 +0900

    [Bronze II] Title: 대표값2, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[5];
+        for (int i=0; i<5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        int avg = (arr[0] + arr[1] + arr[2] + arr[3] + arr[4])/5;
+        System.out.println(avg);
+        System.out.println(arr[2]);
+    }
+}
+
