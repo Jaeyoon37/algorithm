e0ff064630eb280f3297c6c06eec97cc040e75e9
JeongJaeYoon <jyoon3741@naver.com>
2025-05-27 14:11:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.30 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,18 +1,18 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
-        for (String name : completion) {
-            //해당 이름이 이미 있으면 기존값+1, 없으면 0+1
-            hashMap.put(name, hashMap.getOrDefault(name, 0) + 1);
-        }
         for (String name : participant) {
-            //완주 기록이 없거나 개수가 0이면 완주하지 못한 사람으로 간주
-            if (!hashMap.containsKey(name) || hashMap.get(name) == 0) {
-                return name;
+            map.put(name, map.getOrDefault(name,0) + 1);
+        }
+        for (String name: completion) {
+            map.put(name, map.get(name) - 1);
+        }
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return key;
             }
-            hashMap.put(name, hashMap.get(name)-1);
         }
         
         return "";
